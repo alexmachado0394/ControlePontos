6d834e186729b9ee5964120e3064fb1b52cbab4f
alexandremachado94@gmail.com <alexandremachado94@gmail.com>
2020-10-01 10:17:29 -0300

    Cadastro dos resultados e apresentação dos mesmos na tela, porém sem menus.

---
+++ ControlePontos/Classes/Jogo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlePontos.Classes
+{
+    class Jogo
+    {
+        public int Id { get; set; }
+        public int Placar { get; private set; }
+        public int PiorResultado { get; private set; }
+        public int MelhorResultado { get; private set; }
+        public int MudouPior { get; private set; }
+        public int MudouMelhor { get; private set; }
+
+        public Jogo()
+        {
+
+        }
+
+        public Jogo(int id, int placar)
+        {
+            Id = id;
+            Placar = placar;
+        }
+
+        public void ResultadoRuim(int anterior)
+        {
+            if (anterior != 0)
+            {
+                if (anterior < Placar)
+                {
+                    PiorResultado = anterior;
+                }
+                else
+                {
+                    PiorResultado = Placar;
+                }
+            }
+            else PiorResultado = Placar;
+        }
+
+        public void ResultadoBom(int anterior)
+        {
+            if (anterior != 0)
+            {
+                if (anterior > Placar)
+                {
+                    MelhorResultado = anterior;
+                }
+                else
+                {
+                    MelhorResultado = Placar;
+                }
+            }
+            else MelhorResultado = Placar;
+
+        }
+
+        public void MudouResultadoRuim(int anterior, int mudou)
+        {
+            if (Id > 1)
+            {
+                if (anterior > Placar)
+                {
+                    MudouPior = mudou + 1;
+                }
+                else MudouPior = mudou;
+            }
+        }
+
+        public void MudouResultadoBom(int anterior, int mudou)
+        {
+            if (Id > 1)
+            {
+                if (anterior < Placar)
+                {
+                    MudouMelhor = mudou + 1;
+                }
+                else MudouMelhor = mudou;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Id + " , " + Placar + " , " + PiorResultado + " , " + MelhorResultado + " , " + MudouPior + " , " + MudouMelhor;
+        }
+    }
+}
--- ControlePontos/Program.cs
+++ ControlePontos/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlePontos.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,42 @@ namespace ControlePontos
     {
         static void Main(string[] args)
         {
+            int jogos;
+            Console.Write("quantos jogos serão informados?");
+            jogos = int.Parse(Console.ReadLine());
+            List<Jogo> list = new List<Jogo>();
+            int resultado;
+            int pior = 0;
+            int melhor = 0;
+            int mudouBom = 0;
+            int mudouRuim = 0;
+
+            for (int i = 1; i <= jogos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Jogo #{i}:");
+                Console.Write("Resultado: ");
+                resultado = int.Parse(Console.ReadLine());
+                list.Add(new Jogo(i, resultado));
+                Jogo j = list.Find(x => x.Id == i);
+                j.ResultadoRuim(pior);
+                j.MudouResultadoRuim(pior,mudouRuim);
+                mudouRuim = j.MudouPior;
+                pior = j.PiorResultado;
+                j.ResultadoBom(melhor);
+                j.MudouResultadoBom(melhor,mudouBom);
+                mudouBom = j.MudouMelhor;
+                melhor = j.MelhorResultado;
+            }
+
+            Console.WriteLine("Id, Resultado, Pior, Melhor, Piorou, Melhorou");
+            foreach (Jogo item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
